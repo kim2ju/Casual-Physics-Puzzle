08699ff28b66fd4ed3c709668e51e46877e389c4
kim2ju <bamtori05@naver.com>
2023-08-24 02:52:11 +0900

    3. 물리이벤트로 동글 합치기

--- Assets/Scripts/Dongle.cs
+++ Assets/Scripts/Dongle.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class Dongle : MonoBehaviour
 {
+    public GameManager manager;
     public int level;
     public bool isDrag;
+    public bool isMerge;
+
     Rigidbody2D rigid;
+    CircleCollider2D circle;
     Animator anim;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        circle = GetComponent<CircleCollider2D>();
         anim = GetComponent<Animator>();
     }
 
@@ -52,5 +57,77 @@ public class Dongle : MonoBehaviour
     {
         isDrag= false;
         rigid.simulated = true;
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Dongle")
+        {
+            Dongle other = collision.gameObject.GetComponent<Dongle>();
+
+            if (level ==  other.level && !isMerge && !other.isMerge && level < 7)
+            {
+                float meX = transform.position.x;
+                float meY = transform.position.y;
+                float otherX = other.transform.position.x;
+                float otherY = other.transform.position.y;
+
+                if (meY < otherY || (meY == otherY && meX > otherX))
+                {
+                    other.Hide(transform.position);
+                    LevelUp();
+                }
+            }
+        }
+    }
+
+    public void Hide(Vector3 targetPos)
+    {
+        isMerge = true;
+
+        rigid.simulated = false;
+        circle.enabled = false;
+
+        StartCoroutine(HideRoutine(targetPos));
+
+    }
+
+    IEnumerator HideRoutine(Vector3 targetPos)
+    {
+        int frameCount = 0;
+
+        while(frameCount < 20)
+        {
+            frameCount++;
+            transform.position = Vector3.Lerp(transform.position, targetPos, 0.5f);
+            yield return null;
+        }
+
+        isMerge = false;
+        gameObject.SetActive(false);
+    }
+
+    void LevelUp()
+    {
+        isMerge = true;
+
+        rigid.velocity = Vector2.zero;
+        rigid.angularVelocity = 0;
+
+        StartCoroutine (LevelUpRoutine());
+    }
+
+    IEnumerator LevelUpRoutine()
+    {
+        yield return new WaitForSeconds(0.2f);
+
+        anim.SetInteger("Level", level + 1);
+
+        yield return new WaitForSeconds(0.3f);
+        level++;
+
+        manager.maxLevel = Mathf.Max(level, manager.maxLevel);
+
+        isMerge = false;
     }
 }
